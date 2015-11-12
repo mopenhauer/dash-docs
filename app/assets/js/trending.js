ec15a18d1a3508e13a65d65c02121751b9dd32c7
Rebecca Cremona <rebecca_cremona@harvard.edu>
2015-11-12 14:20:36 -0500

    Fix indentation.

--- app/assets/js/trending.js
+++ app/assets/js/trending.js
@@ -5,15 +5,15 @@ $.ajax({
     jsonp: "callback",
     jsonpCallback: 'callback',
     success: function( response ) {
-        var list = $("ol.trending-works");
-        var handle_pattern = new RegExp("1\/[0-9]*");
-        $.each(response, function(index, item){
-          if (handle_pattern.test(item.handle)){
-            var url = "https://dash.harvard.edu/handle/" + item.handle;
-            var li = $("<li>");
-            var a = $("<a>").attr("href", url).text(item.title);
-            li.append(a).appendTo(list);
-          }
-        });
+      var list = $("ol.trending-works");
+      var handle_pattern = new RegExp("1\/[0-9]*");
+      $.each(response, function(index, item){
+        if (handle_pattern.test(item.handle)){
+          var url = "https://dash.harvard.edu/handle/" + item.handle;
+          var li = $("<li>");
+          var a = $("<a>").attr("href", url).text(item.title);
+          li.append(a).appendTo(list);
+        }
+      });
     }
 });
